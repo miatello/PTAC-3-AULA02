5b44a7f97faf3b4d76d0cd8c4573e15fbbe41cd4
rahmartins01 <miatellogmonteiro@gmail.com>
2024-03-27 17:15:47 -0400

    4m
    4m

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -19,6 +19,9 @@ export default function Menu() {
           <Link href="registro">
             <li>Registrar</li>
           </Link>
+          <Link href="/localizacao">
+            <li>Localização</li>
+            </Link>
         </ul>
       </nav>
     </header>
--- src/localizacao/page.js
+++ src/localizacao/page.js
@@ -1,16 +1,10 @@
 import React from "react";
+import Map from "./components/Map"
 
 export default function Map() {
-  return (
-    <iframe
-      id="gmap_canvas"
-      src={
-        "https://maps.google.com/maps?       q=Nova%20Andradina%20+IFMS+&t=&z=13&ie=UTF8&iwloc=&output=embed"
-      }
-      frameborder="0"
-      scrolling="no"
-      marginheight="0"
-      marginwidth="0"
-    ></iframe>
-  );
+  return(
+    <main>
+        <Map/>
+    </main>
+)
 }
